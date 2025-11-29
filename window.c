133ee41e116428addfaf05082ff4b5298cfad81b
Corentin Viel <cviel@z4r5p2.42lyon.fr>
2025-11-29 17:24:57 +0100

    [MOD] init_window should be good for now

--- window.c
+++ window.c
@@ -3,10 +3,21 @@
 
 int init_window(WINDOW **ptr_win, t_info info)
 {
+    int ret;
+    int i;
+    
     *ptr_win = newwin(0,0,0,0);
     if (*ptr_win == NULL)
         return (ERROR_NCURSES);
-    mvprintw(0, 0, "Remaning lives : %d\tElapsed time : %d:%d\tCurrent score : %d",
-            *info.ptr_player_lives, time.se);
+    ret = mvprintw(0, 0, "Remaning lives : %d\tElapsed time : %d:%d\tCurrent score : %d",
+            *info.ptr_player_lives, info.time.tv_sec / 60, info.time.tv_sec % 60, info.score);
+    if (ret == ERR)
+        return (ERROR_NCURSES);
+    i = 0;
+    while (i < COLS)
+    {
+        ret = mvprintw(1, i, "_");
+        ++i;
+    }
     return (SUCCESS);
 }
